13a518b6c3bcdf9a553778db2ecb988256f511a7
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-05-27 09:47:25 -0400

    Process bushman data into separate files

--- getBushman.R
+++ getBushman.R
@@ -1,9 +1,9 @@
 library(dnar)
 
 seqs<-read.fa('data/bushman/seqs.fna.gz',assumeSingleLine=TRUE)
-seqs$sample<-sub('_[0-9]+$','',seqs$name)
+seqs$sample<-sub('_[0-9].*','',seqs$name)
 for(ii in unique(seqs$sample)){
 	message(ii)
-	write.fa(seqs$name,seqs$seq,sprintf('data/bushman/split/%s.fa.gz',ii))
+	write.fa(seqs[seqs$sample==ii,'name'],seqs[seqs$sample==ii,'seq'],sprintf('data/bushman/split/%s.fa.gz',ii))
 }
 

32f440212b6c3c6453a94d4977bd9b575218adc6
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-05-31 09:30:42 -0400

    Small cleanup

--- swarmBushman.bash
+++ swarmBushman.bash
@@ -1,16 +1,11 @@
 #!/bin/bash
-##File name: swarmData.bash
-##Creation date: Aug 24, 2015
-##Last modified: Fri May 27, 2016  10:00AM
-##Created by: scott
-##Summary: Swarm to make OTUs
 
 cd data/bushman/split/
 
 echo Starting filtering
 for ii in *.fa.gz;do 
 	echo $ii
-	sem -j8  Rscript ../../../uniquify.R $ii ${ii%.fa.gz}_uniq.fa 250 350 #sem from apt-get install parallel 
+	sem -j8 Rscript ../../../uniquify.R $ii ${ii%.fa.gz}_uniq.fa 250 350 #sem from apt-get install parallel 
 done
 sem --wait
 echo Done filtering
@@ -18,7 +13,7 @@ echo Done filtering
 echo Starting swarm
 for ii in *_uniq.fa;do 
 	echo $ii
-	sem -j8  ~/installs/swarm/swarm $ii -o ${ii%.fa}.out
+	sem -j8 ~/installs/swarm/swarm $ii -o ${ii%.fa}.out
 done
 sem --wait
 echo Done swarming

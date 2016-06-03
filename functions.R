e411246c47accefa0375ab6cd969e154e97962ca
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-06-03 17:02:29 -0400

    Swarm running function

---
+++ functions.R
@@ -0,0 +1,18 @@
+runSwarm<-function(seqs,swarmBin='swarm',swarmArgs='-f'){
+	seqIds<-as.numeric(as.factor(seqs))
+	seqCounts<-ave(seqIds,seqIds,FUN=length)
+	seqNames<-sprintf('%08d_%d',seqIds,seqCounts)
+	readFile<-tempfile()
+	outFile<-tempfile()
+	uniqSelector<-!duplicated(seqs)
+	write.fa(seqNames[uniqSelector],seqs[uniqSelector],readFile)
+	cmd<-sprintf('%s %s %s -o %s',swarmBin,swarmArgs,readFile,outFile)
+	system(cmd)
+	swarm<-readLines(outFile)
+	otuSplit<-strsplit(swarm,' ')
+	otus<-rep(1:length(otuSplit),sapply(otuSplit,length))
+	names(otus)<-unlist(otuSplit)	
+	out<-otus[seqNames]
+	return(out)
+}
+runSwarm(c('AAA','AAA','ATA','TTT','AAA','CCC'),swarmBin='~/installs/swarm/swarm')

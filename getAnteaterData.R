f1c7403772ad0adbbc7a322a88db2bfd45d9aff3
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-06-07 12:41:57 -0400

    Clean up get data scripts

--- getAnteaterData.R
+++ getAnteaterData.R
@@ -6,8 +6,8 @@ sampleUrl<-'http://www.ebi.ac.uk/ena/data/view/%s&display=xml'
 info$species<-sapply(sprintf(sampleUrl,info$sample_accession),function(address)sub('fecal sample from ','',xpathSApply(xmlParse(address),'//DESCRIPTION',xmlValue)))
 info$file<-basename(info$fastq_ftp)
 write.csv(info[,c('run_accession','species','file')],'data/anteater/info.csv',row.names=FALSE)
-
+info$target<-sprintf('data/anteater/%s',basename(x))
 
 #download files
-sapply(info$fastq_ftp,function(x)if(!file.exists(sprintf('data/anteater/%s',basename(x))))download.file(sprintf('http://%s',x),sprintf('data/anteater/%s',basename(x))))
+mapply(function(xx,yy)if(!file.exists(yy))download.file(sprintf('http://%s',xx),yy),info$fastq_ftp,info$target)
 

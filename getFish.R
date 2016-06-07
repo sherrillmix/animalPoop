b37c12c59708628ff31e08af5f7e47e97d44da1a
sherrillmix <sherrillmix@users.noreply.github.com>
2016-06-06 22:21:37 -0400

    Find SRR number

--- getFish.R
+++ getFish.R
@@ -3,9 +3,23 @@
 #sample info from SRA
 ncbi<-readLines('data/fish/biosample_result.txt')
 sraLines<-grep('SRA:',ncbi)
+idLines<-grep('original_id=',ncbi)
 hostLines<-grep('/host=',ncbi)
-lineDiff<-hostLines-sraLines
-if(any(lineDiff!=lineDiff[1]))stop(simpleError('Host and sra lines not consistent'))
-fish<-data.frame('sra'=sub('.*SRA: ','',ncbi[sraLines]),'species'=sub('.*\\host=','',ncbi[hostLines]),stringsAsFactors=FALSE)
+lineDiff<-hostLines-idLines
+if(any(lineDiff!=lineDiff[1]))stop(simpleError('Host and id lines not consistent'))
+
+fishFirstLines<-sapply(list.files('data/fish','fastq$',full.names=TRUE),readLines,n=1)
+#fishFirstLines<-sub('^@([^.]+)\\.[0-9].*$','\\1',fishFirstLines)
+fishFirstLines<-sub('^[^ ]+ ([^_]+)_.*$','\\1',fishFirstLines)
+
+
+fish<-data.frame(
+	'srs'=sub('.*SRA: ','',ncbi[sraLines])
+	,'srr'=sub('.*/([^.]+).fastq','\\1',names(fishFirstLines))
+	,'species'=sub('.*\\host="([^"]+)"','\\1',ncbi[hostLines])
+	,'id'=sub('.*\\original_id="([^"]+)"','\\1',ncbi[idLines])
+	,stringsAsFactors=FALSE
+)
+
 write.csv(fish,'data/fish/sampleInfo.csv',row.names=FALSE)
 

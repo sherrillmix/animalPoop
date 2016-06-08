8bcf7e5feedd3821ddc26a6ab0d143b6bac32826
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-06-08 12:03:22 -0400

    Deal with qualities and selectors correctly

--- functions.R
+++ functions.R
@@ -1,4 +1,5 @@
 runSwarm<-function(seqs,swarmBin='swarm',swarmArgs='-f'){
+	if(any(is.na(seqs)))stop(simpleError('NAs in seqs'))
 	seqIds<-as.numeric(as.factor(seqs))
 	seqCounts<-ave(seqIds,seqIds,FUN=length)
 	seqNames<-sprintf('%08d_%d',seqIds,seqCounts)
@@ -7,6 +8,7 @@ runSwarm<-function(seqs,swarmBin='swarm',swarmArgs='-f'){
 	seqFile<-tempfile()
 	uniqSelector<-!duplicated(seqs)
 	write.fa(seqNames[uniqSelector],seqs[uniqSelector],readFile)
+	browser()
 	cmd<-sprintf('%s %s %s -o %s -w %s',swarmBin,swarmArgs,readFile,outFile,seqFile)
 	system(cmd)
 	swarm<-readLines(outFile)
@@ -23,15 +25,16 @@ filterReads<-function(seqs,quals=NULL,minLength=0,maxLength=1e6,minQual=0,maxBad
 	out<-sub('[actgn]+$','',sub('^[actgn]+','',seqs)) #remove trimmed sequence at start and end
 	out<-substring(out,1,maxLength)
 	selector<-!grepl('[^ACTG]',out)
+	comboSelector<-selector
 	message('Throwing out ',sum(!selector),' sequences containing non ACTG (',sum(selector),' remaining)')
-	out<-out[selector]
 	selector<-nchar(out)>=minLength
-	message('Throwing out ',sum(!selector),' sequences shorter than ',minLength,' (',sum(selector),' remaining)')
-	out<-out[selector]
+	message('Throwing out ',sum(comboSelector&!selector),' sequences shorter than ',minLength,' (',sum(comboSelector&selector),' remaining)')
+	comboSelector<-comboSelector&selector
 	if(minQual>0&!is.null(quals)){
 		selector<-sapply(strsplit(quals,' '),function(x)sum(as.numeric(x)<minQual))<=maxBadQual
-		message('Throwing out ',sum(!selector),' poor quality sequences (',sum(selector),' remaining)')
-		out<-out[selector]
+		message('Throwing out ',sum(comboSelector&!selector),' poor quality sequences (',sum(selector&comboSelector),' remaining)')
+		comboSelector<-comboSelector&selector
 	}
+	out<-out[comboSelector]
 	return(out)
 }

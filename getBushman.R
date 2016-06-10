4ce6c36e1227d8f687938685b5e99710f7477bb2
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-06-10 10:12:21 -0400

    Run swarm on pooled bushman

--- getBushman.R
+++ getBushman.R
@@ -2,8 +2,14 @@ library(dnar)
 
 seqs<-read.fa('data/bushman/raw/seqs.fna.gz',assumeSingleLine=TRUE)
 seqs$sample<-sub('_[0-9].*','',seqs$name)
-for(ii in unique(seqs$sample)){
-	message(ii)
-	write.fa(seqs[seqs$sample==ii,'name'],seqs[seqs$sample==ii,'seq'],sprintf('data/bushman/%s.fa.gz',ii))
-}
 
+allSeqs<-tapply(seqs$seq,seqs$sample,function(xx)filterReads(xx,minLength=250,maxLength=350))
+
+tmp<-runSwarm(unlist(allSeqs),swarmBin='~/installs/swarm/swarm',swarmArgs='-f -t 32')
+otus<-tmp[['otus']]
+seqs<-tmp[['seqs']]
+samples<-rep(names(allSeqs),sapply(allSeqs,length))
+dir.create('work/data/bushman',showWarnings=FALSE)
+write.fa(1:length(seqs),seqs,'work/data/bushman/swarmSeqs.fa.gz')
+otuTab<-table(samples,otus)
+write.csv(otuTab,'work/data/bushman/otuTab.csv')

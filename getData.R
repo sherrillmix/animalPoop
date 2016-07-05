79a2aec615abda7ea34ac9910d145190d5e43be5
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-07-05 08:19:42 -0400

    Metascript to import data

---
+++ getData.R
@@ -0,0 +1,12 @@
+
+source('getAnteaterData.R')
+rm(list=ls())
+source('getBushman.R')
+rm(list=ls())
+source('getFish.R')
+rm(list=ls())
+source('getMgrastData.R')
+rm(list=ls())
+source('getMgrastWhaleData.R')
+rm(list=ls())
+

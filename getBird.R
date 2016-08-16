3eb4f577b4c6e681c6a9a6fc8947855593ef36ca
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-08-16 08:08:59 -0400

    Adjust birds to kilograms

--- getBird.R
+++ getBird.R
@@ -5,7 +5,7 @@ source('functions.R')
 #http://www.ncbi.nlm.nih.gov/pmc/articles/PMC4685052/
 
 info<-read.table(textConnection(sub('^#','',readLines('data/bird/cP315.mapping.txt'))),sep='\t',header=TRUE,stringsAsFactors=FALSE)
-info$weight<-as.numeric(ifelse(info$Weightg=='nr',NA,info$Weightg))
+info$weight<-as.numeric(ifelse(info$Weightg=='nr',NA,info$Weightg))/1000
 info$sample<-info$SampleID
 info$species<-paste(info$AOU_genus,info$AOU_species)
 info$name<-info$SampleID

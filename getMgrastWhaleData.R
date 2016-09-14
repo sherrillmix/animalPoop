bfe433f6b14fe823296b173e9407db253bd083a4
sherrillmix <sherrillmix@users.noreply.github.com>
2016-09-14 09:09:26 -0400

    Parse updated whale download. Process illumina and 454 seperately

--- getMgrastWhaleData.R
+++ getMgrastWhaleData.R
@@ -1,6 +1,6 @@
 ##File name: getMgrastData.R
 ##Creation date: Aug 27, 2015
-##Last modified: Fri Sep 02, 2016  09:00AM
+##Last modified: Wed Sep 14, 2016  08:00AM
 ##Created by: scott
 ##Summary: Download Whale data from MGRAST. More difficult that it should be
 
@@ -11,6 +11,7 @@ source('functions.R')
 library(dnar)
 
 projectIds<-3854
+sequencings<-c('whale454'='16S','whaleIllumina'='16S.Ilm.V4')
 #The awesome mg-rast javascript in the table so we cant scrape it. saving the pages manually
 #ftp://ftp.metagenomics.anl.gov/data/manual/mg-rast-manual.pdf
 #ftp://ftp.metagenomics.anl.gov/projects/README.ftp
@@ -56,16 +57,25 @@ info$file<-sprintf('data/mgrast/%s.fastq.gz',info$MG.RAST.ID)
 weights<-read.csv('data/mgrast/mgrastWhaleWeights.csv',stringsAsFactors=FALSE) #rough estimates from wikipedia and http://marinebio.org/species.asp?id=192 and http://www.esf.edu/aec/adks/mammals/wtd.htm
 rownames(weights)<-weights$name
 info$animal<-sapply(strsplit(info$Metagenome.Name,'\\.'),'[[',1)
+info$sequencing<-sub('.*16S','16S',info$Metagenome.Name)
+if(any(!info$sequencing %in% sequencings))stop(simpleError('Unknown sequencing'))
 info<-cbind(info,weights[info$animal,-1])
-
-dir.create('work/data/whale',showWarnings=FALSE)
 info$name<-info$file
-write.csv(info,'work/data/whale/info.csv')
 
-allSeqs<-mclapply(info$file,function(x){
-	tmp<-read.fastq(x,convert=TRUE)
-	seqs<-filterReads(tmp$seq,tmp$qual,minLength=250,maxLength=350,minQual=10,maxBadQual=3)
-	return(seqs)
-},mc.cores=16)
+for(ii in names(sequencings)){
+  message(ii)
+  outDir<-file.path('work/data',ii)
+  dir.create(outDir,showWarnings=FALSE)
+  selector<-info$sequencing==sequencings[ii]
+  write.csv(info[selector,],file.path(outDir,'info.csv'))
 
-runOtuForming(unlist(allSeqs),rep(info$file,sapply(allSeqs,length)),'work/data/whale')
+  allSeqs<-mclapply(info$file[selector],function(x){
+    tmp<-read.fastq(x,convert=TRUE)
+    if(ii=='whale454')seqs<-filterReads(tmp$seq,tmp$qual,minLength=250,maxLength=350,minQual=10,maxBadQual=3)
+    else if(ii=='whaleIllumina')seqs<-filterReads(tmp$seq,tmp$qual,minLength=200,minQual=20,maxBadQual=3)
+    else stop(simpleError('Unknown sequencing'))
+    return(seqs)
+  },mc.cores=16)
+
+  runOtuForming(unlist(allSeqs),rep(info$file[selector],sapply(allSeqs,length)),outDir)
+}
